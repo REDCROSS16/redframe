8bb3bc740ed83eaa581ac8fd208f703f7f982051
redcross16 <belkill@mail.ru>
2022-01-02 23:58:59 +0300

    update RedFrame

--- project/views/file/upload.php
+++ project/views/file/upload.php
@@ -1,4 +1,13 @@
+<h2>Page to upload image</h2>
 <form action="" enctype="multipart/form-data">
-    <input type="file">
-    <input type="submit">
+    <input type="file" name="file">
+    <input type="submit" value="загрузить файл">
 </form>
+
+<?php
+
+$file = new \project\controllers\FileController();
+
+if (isset($_FILES['file'])) {
+
+}
